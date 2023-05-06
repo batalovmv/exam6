67bd21c05682594dd9285b37a7d87adbfa008006
Баталов Михаил <batalov94@gmail.com>
2023-05-06 15:55:59 +0600

    #3 added new files, droped code

---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import * as readlineSync from "readline-sync";
+import { trucksLog, trucksLogId, products, StateData } from "./mainData";
+
+
+for (let i: boolean = false; i === false;) {
+  const menu = Number(readlineSync.question('1.Показать все товары\n2.Показать данные товара по id\n3.Произвести манипуляции с товаром\n4.Завершить программу\n '))
+  switch (menu) {
+    case 1:
+      trucksLog()
+      break;
+    case 2:
+      const idNumber = Number(readlineSync.question('Введите id товара\n '))
+      trucksLogId(idNumber)
+      break;
+    case 3:
+      const changeState = readlineSync.question('Введите id  и желаемый статус ( raise , up , off , winner ) через пробел\n ')
+      const newWords = changeState.trim().replace(/\s+/g, ' ').split(' ')
+      if (Number(newWords[0]) > 0 && Number(newWords[0]) <= products.length && typeof Number(newWords[0]) as 'number') {
+        let a = new StateData(Number(newWords[0]))
+
+
+        if (newWords[1] === 'raise') {
+          const changePrice = Number(readlineSync.question('Введите цену\n '))
+          if (changePrice > 0) {
+            a.raise(changePrice)
+            console.log('Цена повышена на ' + changePrice);
+
+          } else {
+            console.log('Введено меньше нуля или не число');
+
+          }
+          console.log('Вы выбрали raise');
+
+        } else if (newWords[1] === 'up') {
+          console.log('Вы выбрали up');
+          a.up()
+        } else if (newWords[1] === 'off') {
+          console.log('Вы выбрали off');
+          a.off()
+        } else if (newWords[1] === 'winner') {
+          console.log('Вы выбрали winner');
+          a.winner()
+        } else {
+
+          console.log('Некорректный ввод действия');
+        }
+        break
+      } else {
+        console.log('Некорректный ввод');
+        break
+      }
+    case 4:
+      i = true
+      break;
+    default:
+      console.log('Введеное число не найдено в меню');
+      break
+
+  }
+}
--- src/mainData.ts
+++ src/mainData.ts
@@ -1,6 +1,5 @@
-import * as readlineSync from "readline-sync";
-import MD5 from "./MD5";
-import { goldStrategy, bronzeStrategy, silverStrategy, ProductStatus, newCode } from "./strategy";
+
+import { newCode } from "./strategy";
 abstract class IProduct {
   id: number
   name: string
@@ -9,9 +8,7 @@ abstract class IProduct {
   state: State
   stringState: string
 }
-
 export class Product extends IProduct {
-
   constructor(id: number, name: string, price: number) {
     super()
     this.id = id
@@ -19,7 +16,6 @@ export class Product extends IProduct {
     this.price = price
     this.stringState = 'on base'
     this.honoraryСode = 'none'
-
   }
 }
 
@@ -51,7 +47,7 @@ export class StateData extends IProduct {
     products[id - 1] = this
     this.changeState(this.state)
     products[id - 1].honoraryСode = this.honoraryСode
-  
+
 
 
 
@@ -114,11 +110,11 @@ class ForSaleState extends State {
   public giveToTheWinner(): void {
     if (this.stateData.price === 0) console.log('Нельзя отдать продукт бесплатно');
     else {
-            this.stateData.changeStringState('sold')
+      this.stateData.changeStringState('sold')
       this.stateData.changeState(new SoldState())
       this.stateData.changeCode(newCode(this.stateData.id))
       console.log(this.stateData.honoraryСode);
-  
+
     }
   }
   public setOff(): void {
@@ -145,13 +141,13 @@ class SoldState extends State {
 const apple = new Product(1, "Rapple", 500);
 const apple2 = new Product(2, "samsung", 100);
 const apple3 = new Product(3, "xiaomi", 999999);
-const products: Product[] = [apple, apple2, apple3];
-function trucksLog() {
+export const products: Product[] = [apple, apple2, apple3];
+export function trucksLog() {
   products.forEach(element => {
     console.log(`${element.id} | ${element.name} | ${element.price} | ${element.stringState} |  ${element.honoraryСode}`)
   });
 }
-function trucksLogId(id: number) {
+export function trucksLogId(id: number) {
   if (id <= products.length && id > 0) {
     for (let i = 0; i < products.length; i++) {
       if (products[i].id === id) {
@@ -159,85 +155,11 @@ function trucksLogId(id: number) {
         break
       }
     }
-
   } else {
     console.log('Введено неправильное число');
-
   }
 }
 
-
-
-
-
-
-
-
-
-
-
-for (let i: boolean = false; i === false;) {
-  const menu = Number(readlineSync.question('1.Показать все товары\n2.Показать данные товара по id\n3.Произвести манипуляции с товаром\n5.Завершить программу\n '))
-  switch (menu) {
-    case 1:
-      trucksLog()
-      break;
-    case 2:
-      const idNumber = Number(readlineSync.question('Введите id товара\n '))
-      trucksLogId(idNumber)
-      console.log(products[idNumber - 1]);
-      break;
-    case 3:
-      const changeState = readlineSync.question('Введите id  и желаемый статус ( raise , up , off , winner ) через пробел\n ')
-      const newWords = changeState.trim().replace(/\s+/g, ' ').split(' ')
-      if (Number(newWords[0]) > 0 && Number(newWords[0]) <= products.length && typeof Number(newWords[0]) as 'number') {
-        let a = new StateData(Number(newWords[0]))
-        
-
-        if (newWords[1] === 'raise') {
-          const changePrice = Number(readlineSync.question('Введите цену\n '))
-          if (changePrice > 0) {
-            a.raise(changePrice)
-            console.log('Цена повышена на '+changePrice);
-            
-          }else{
-            console.log('Введено меньше нуля или не число');
-            
-          }
-          console.log('Вы выбрали raise');
-
-        } else if (newWords[1] === 'up') {
-          console.log('Вы выбрали up');
-          a.up()
-        } else if (newWords[1] === 'off') {
-          console.log('Вы выбрали off');
-          a.off()
-        } else if (newWords[1] === 'winner') {
-          console.log('Вы выбрали winner');
-          a.winner()
-        } else {
-
-          console.log('Некорректный ввод действия');
-        }
-        break
-      } else {
-        console.log('Некорректный ввод');
-        break
-      }
-    // case 4:
-    //   trucks.push(newTruck())
-    //   console.log('Успешно сгенерирован новый водитель');
-
-    //   break
-    case 5:
-      i = true
-      break;
-    default:
-      console.log('Введеное число не найдено в меню');
-      break
-
-  }
-}
 
 
 
